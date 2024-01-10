7aeb3f46e1ef84bd575699ed5f50ba0e01faa7c9
Sergey <32293129+SergeyShehovtcov@users.noreply.github.com>
2024-01-10 07:33:36 +0000

    Домашняя работа №6. Начало. Сделал форму регистрации

---
+++ src/components/Forms/RegisterForm/RegisterForm.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { Meta } from '@storybook/react';
+import { RegisterForm } from './RegisterForm';
+
+const meta: Meta<typeof RegisterForm> = {
+  title: 'Компоненты/Forms/RegisterForm',
+  component: RegisterForm,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Форма_регистрации = {};
